6f222654d2217ae05e556466f6cb976abe3cbe58
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-07-30 23:23:28 -0500

    manager tests fail

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,3 +1,14 @@
 const Manager = require('../lib/Manager.js');
 
-// creates manage
+// creates manager object
+test('creates a manager object', () => {
+    const manager = new Manager('Michael', '13', 'michael@email.com', '1');
+
+    expect(manager.officeNumber).toBe('1');
+});
+
+// get 'manager' from getRole()
+test("get's employee's role", () => {
+    const manager = new Manager('Michael');
+    expect(manager.getRole()).toBe('Manager');
+});
